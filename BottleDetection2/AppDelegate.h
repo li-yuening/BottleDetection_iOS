00113417fa25cf2c2b203f9a24b31a5e31296740
Li Yuening <Leo2014@LideMacBook-Pro.local>
2015-05-02 18:38:07 +0800

    Initial Commit

---
+++ BottleDetection2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BottleDetection2
+//
+//  Created by Li Yuening on 15/5/2.
+//  Copyright (c) 2015年 Li Yuening. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BottleDetection2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BottleDetection2
+//
+//  Created by Li Yuening on 15/5/2.
+//  Copyright (c) 2015年 Li Yuening. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
