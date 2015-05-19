df9959a5a68fa65dea537fb4718aeb3c20908ec2
tontwen-lyn <Leo2014@LideMacBook-Pro.local>
2015-05-19 19:48:08 +0800

    add custom cell

---
+++ BottleDetection2/AllBottleCustomCell.h
@@ -0,0 +1,15 @@
+//
+//  AllBottleCustomCell.h
+//  BottleDetection2
+//
+//  Created by Li Yuening on 15/5/19.
+//  Copyright (c) 2015年 Li Yuening. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AllBottleCustomCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *bottleNumberLabel;
+@property (weak, nonatomic) IBOutlet UILabel *carNumberLabel;
+
+@end
